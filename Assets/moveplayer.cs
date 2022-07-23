07ebc5d82b4b02c895afdc440b7425be45737aa7
McPlesk <tomas@blazkovi.eu>
2022-07-24 01:09:52 +0200

    Initial commit

---
+++ Assets/moveplayer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class moveplayer : MonoBehaviour
+{
+    public static string havekey = "n";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal")*4, Input.GetAxis("Vertical")*4);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if (other.tag == "key"){
+            havekey = "y";
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D door){
+        if ((door.gameObject.tag == "door") && (havekey == "y")){
+            SceneManager.LoadScene("winscreen");
+        }
+    }
+}
